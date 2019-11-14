d15984b3df3406f15e1240eca8aa78bb3f51728d
Phani Mashetti <Phani.Mashetti@xpo.com>
2019-11-14 01:42:01 -0800

    add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalculatePegRatioComponent } from './component/calculate-peg-ratio/calculate-peg-ratio.component';
+import {StocksApiService} from "./service/stocks-api.service";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatSliderModule
+} from "@angular/material";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatePegRatioComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    StocksApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
