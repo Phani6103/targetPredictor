65d93c2ba1b5847332c21a113a1d7f494520ebcc
Phani Mashetti <Phani.Mashetti@xpo.com>
2020-01-08 18:27:30 -0800

    Add Route navigation from stocks list to stock form

--- src/app/stocks/stocks-list/stocks-list.component.ts
+++ src/app/stocks/stocks-list/stocks-list.component.ts
@@ -7,33 +7,26 @@ import {Router} from '@angular/router';
   templateUrl: './stocks-list.component.html',
   providers: [StocksService]
 })
-export class StocksListComponent implements OnInit, AfterViewInit {
+export class StocksListComponent implements OnInit {
   stocks;
   sel;
   constructor(private stocksService: StocksService,
               private router: Router) {
-    // [options]
-    // .addEventListener('selected', (evt) => {
-    //   console.log('evt: ', evt);
-    // });
-  }
-
-  ngAfterViewInit(): void {
 
 
   }
 
   ngOnInit(): void {
+    document.getElementById('stocksDataListInput')
+      .addEventListener('input', (evt: Event) => {
+        this.router.navigate([`/stocks/${(evt.target as HTMLInputElement).value}`]);
+      });
+
     this
       .stocksService
       .getStocksList('US')
       .subscribe(resp => {
         this.stocks = resp;
       });
-  }
-
-  onStockSelected() {
-    const stockSel = (document.getElementById('stocksDataListInput') as HTMLInputElement).value;
-    this.router.navigate([`/stocks/${stockSel}`]);
   }
 }
